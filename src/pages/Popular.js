080c271ccc9b70e95952a28a336609b1a5486888
JongCHONG <yonhoa27@hotmail.fr>
2021-11-15 17:30:25 +0100

    recuperation des données

--- src/pages/Popular.js
+++ src/pages/Popular.js
@@ -12,12 +12,12 @@ class Popular extends Component {
   componentDidMount() {
     fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=74ff4d5b18f55c304a239fadf716fe2f")
       .then(response => response.json()) // on transforme la donnée reçue en JSON 
-      .then(result => { this.setState({movie: result}) }) // on détaille l'action à exécuter sur ce JSON
+      .then(result => { this.setState({movie: result.results}) }) // on détaille l'action à exécuter sur ce JSON
   }
 
   render() {
     console.log(this.state.movie)
-
+    console.log(this.state.movie[0])
     return (
       <div>
         <h1>Popular</h1>
