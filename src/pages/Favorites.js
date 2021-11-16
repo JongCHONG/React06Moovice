ae16dc4003c0e64f0d2518f0b31ef2163674e1b6
JongCHONG <yonhoa27@hotmail.fr>
2021-11-16 17:24:58 +0100

    getmovie error

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import Card from '../components/Card'
+
 class Favorites extends Component {
   constructor() {
     super()
@@ -10,21 +12,45 @@ class Favorites extends Component {
     }
 
     this.getStorage = this.getStorage.bind(this)
+    this.getMovie = this.getMovie.bind(this)
+  }
+
+  componentDidMount() {
+    this.state.favIDs.forEach(element => {
+      fetch(this.getMovie(element))
+        .then(response => response.json()) // on transforme la donnée reçue en JSON 
+        .then(result => { this.setState({movie: result}) }) // on détaille l'action à exécuter sur ce JSON
+    })
   }
 
   getStorage() {
     const favorite = localStorage.getItem('favorites')
-    console.log(favorite);
     const array = JSON.parse(favorite)
     return array
+  }
+  getMovie(id){
+    const getMovieLink = `https://api.themoviedb.org/3/movie/${id}?api_key=74ff4d5b18f55c304a239fadf716fe2f`
+    return getMovieLink
   }
 
   render() {
     console.log(this.state)
     return (
-      <div>
-        <h1>Favorites</h1>
-          
+      <div className="container">
+        <h1>Favorite</h1>
+        <div className="row">
+          {this.state.movie.map(element => {
+            return (
+              <Card 
+                key={element.id} 
+                movieTitle={element.title} 
+                movieOverview={element.overview} 
+                movieReleaseDate={element.release_date}
+                moviePosterPath={element.poster_path}
+              />
+            )
+          })}
+        </div>
       </div>
     )
   }
